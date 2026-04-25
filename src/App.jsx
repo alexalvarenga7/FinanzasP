b7dd04439f50bf278ac8f2cb2b93d88558e68001
alexalvarenga7 <2000zelaya32@gmail.com>
2026-04-25 01:32:55 -0600

    ajustes a todas las paginas del menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
+import { Routes, Route } from 'react-router-dom';
 import Sidebar from './components/Sidebar';
 import Dashboard from './pages/Dashboard';
+import Transacciones from './pages/Transacciones';
+import Reportes from './pages/Reportes';
 
 export default function App() {
   return (
     <div className="flex min-h-screen bg-dash-bg font-sans">
       <Sidebar />
-      <Dashboard />
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/transacciones" element={<Transacciones />} />
+        <Route path="/reportes" element={<Reportes />} />
+      </Routes>
     </div>
   );
 }
